f0ecc6d835e2bbb18db558aa316b4a773026cef4
tenzijn <tenzinkunchok2022@gmail.com>
2024-01-17 22:15:03 +0100

    editor js is added

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,5 +1,18 @@
+import { createReactEditorJS } from 'react-editor-js';
+import { EDITOR_JS_TOOLS } from '../Constant';
+
+const ReactEditorJS = createReactEditorJS();
+
 function Editor() {
-  return <div>Editor</div>;
+  return (
+    <ReactEditorJS
+      tools={EDITOR_JS_TOOLS}
+      defaultValue={{
+        time: new Date().getTime(),
+        blocks: [],
+      }}
+    />
+  );
 }
 
 export default Editor;
