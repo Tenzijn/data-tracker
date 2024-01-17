d1f27ffa56b2d76ead5edd258e8fb32f45d298c6
tenzijn <tenzinkunchok2022@gmail.com>
2024-01-17 21:22:31 +0100

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.scss';
+// React Router
+import { Routes, Route } from 'react-router-dom';
+import './style/App.scss';
+
+// pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Editor from './pages/Editor';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-      <h1>Dear Dairy</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/editor' element={<Editor />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
