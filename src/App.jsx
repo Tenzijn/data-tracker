f25ddd989dafc0665e2e66a47898332505570355
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-01-19 00:41:57 +0100

    firebase added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { Routes, Route } from 'react-router-dom';
 import './style/App.scss';
 
+import { handleSubmit, handleDelete } from './handles/firebaseHandler';
+
 // pages
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -11,6 +13,26 @@ import PageNotFound from './pages/PageNotFound';
 function App() {
   return (
     <>
+      <button
+        onClick={() => {
+          handleSubmit({
+            title: 'test title',
+            content: 'test content',
+            date: 'test date',
+            time: 'test time',
+            uid: 'test uid',
+          });
+        }}
+      >
+        add
+      </button>
+      <button
+        onClick={() => {
+          handleDelete('L2WpTYVl6GHybpgqXALk');
+        }}
+      >
+        delete
+      </button>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
