e07d64c5fc5702781287f7bf7c35e3fed2e95bca
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-01-20 10:51:28 +0100

    firebase added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,35 @@
+import {
+  handleCreateUser,
+  handleGoogleLogin,
+} from '../handles/firebaseHandler';
+
+import '../style/Login.scss';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    // login page
+    <div className='login'>
+      <div className='login__container'>
+        <h1 className='login__title'>Login</h1>
+        <button
+          className='login__button'
+          onClick={() => {
+            handleGoogleLogin();
+          }}
+        >
+          Google
+        </button>
+        <button
+          className='login__button'
+          onClick={() => {
+            handleCreateUser();
+          }}
+        >
+          Create User
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
