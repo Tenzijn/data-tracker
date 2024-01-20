fbde667f2234594736e57e50dee357af48fe632b
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-01-20 15:25:29 +0100

    merged

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <Router>
-        <App />
-      </Router>
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <Router>
+      <App />
+    </Router>
+  </ChakraProvider>
 );
